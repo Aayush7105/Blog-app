8745d5ad880171e06f8a5cd4ff5365d5882f1342
Aayush7105 <aayushrawat5107@gmail.com>
2025-11-24 21:35:09 +0530

    comment route added

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { connectToDatabase } from "@/lib/mongodb";
-import Comment from "@/models/Comment";
+import Comment from "@/models/Comments";
 
 export async function POST(req: Request) {
   await connectToDatabase();
